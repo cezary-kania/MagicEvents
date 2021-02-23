58d2673887f9aea0d2956174c8aa2c5c892ac15d
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-23 10:51:05 +0100

    Restructure solution directories

---
+++ MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MagicEvents.CRUD.Service.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+    }
+}
