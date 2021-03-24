e7aa620bda24db5128e183482828c9c085e8e72a
cezary-kania <cezary.kaniadev@gmail.com>
2021-03-24 22:40:36 +0100

    Implement user repository, add mongo collection

--- MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Infrastructure/MongoDb/Interfaces/IMongoDbSettings.cs
+++ MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Infrastructure/MongoDb/Interfaces/IMongoDbSettings.cs
@@ -3,6 +3,7 @@ namespace MagicEvents.CRUD.Service.Infrastructure.MongoDb.Interfaces
     public interface IMongoDbSettings
     {
         string EventsCollectionName { get; set; }
+        string UsersCollectionName { get; set; } 
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
     }
--- MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Infrastructure/MongoDb/MongoDbSettings.cs
+++ MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Infrastructure/MongoDb/MongoDbSettings.cs
@@ -4,7 +4,8 @@ namespace MagicEvents.CRUD.Service.Infrastructure.MongoDb
 {
     public class MongoDbSettings : IMongoDbSettings
     {
-        public string EventsCollectionName { get; set; }
+        public string EventsCollectionName { get; set; } 
+        public string UsersCollectionName { get; set; } 
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
     }
