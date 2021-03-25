4de049c942ca0e878f3f64a4f8ed8a30006b409f
cezary-kania <cezary.kaniadev@gmail.com>
2021-03-25 21:54:11 +0100

    Implement user service

---
+++ MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Application/Services/UserService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using MagicEvents.CRUD.Service.Application.DTOs.Users;
+using MagicEvents.CRUD.Service.Application.Exceptions;
+using MagicEvents.CRUD.Service.Application.Services.Interfaces;
+using MagicEvents.CRUD.Service.Domain.Repositories;
+
+namespace MagicEvents.CRUD.Service.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        public UserService(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+        public async Task<UserDto> Get(Guid userId)
+        {
+            var user = await _userRepository.GetAsync(userId);
+            if(user is null)
+            {
+                throw new ServiceException(ExceptionMessage.User.UserNotFound);
+            }
+            return _mapper.Map<UserDto>(user);
+        }
+    }
+}
