9899602f81a46714b7ca6152d11ffed8fe22e398
cezary-kania <cezary.kaniadev@gmail.com>
2021-03-27 07:34:25 +0100

    Encapsulate Participant lists

--- MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Domain/Entities/Event.cs
+++ MagicEvents.CRUD.Service/src/MagicEvents.CRUD.Service.Domain/Entities/Event.cs
@@ -8,18 +8,14 @@ namespace MagicEvents.CRUD.Service.Domain.Entities
     public class Event
     {
         public Guid Id { get; protected set; }
-        public Guid OrganizerId { get; set; }
-        public EventParticipants Participants { get; set; }
+        public Guid OrganizerId { get; protected set; }
+        public EventParticipants Participants { get; protected set; }
+        public EventThumbnail Thumbnail { get; protected set; }
         public string Title { get; set; }
         public string Description { get; set;}
-        public EventThumbnail Thumbnail { get; protected set; }
         public DateTime StartsAt { get; set; }
         public DateTime EndsAt { get; set; }
         public string Status { get; set; }
-
-        protected Event()
-        {
-        }
         
         protected Event(Guid id,
                         Guid organizerId,
@@ -35,6 +31,7 @@ namespace MagicEvents.CRUD.Service.Domain.Entities
             StartsAt = startsAt;
             EndsAt = EndsAt;
             Status = EventStatus.Open;
+            Participants = new EventParticipants();
         }
         public void SetThumbnail(byte[] thData)
         {
@@ -42,24 +39,16 @@ namespace MagicEvents.CRUD.Service.Domain.Entities
         }
         public void AddParticipant(Guid userId, string role)
         {
-            if(role == UserEventRole.StandardParticipant)
-            {
-                Participants.StandardParticipants.Add(userId);
-            }
-            else if(role == UserEventRole.CoOrganizer)
-            {
-                Participants.CoOrganizers.Add(userId);
-            }
+            Participants.AddParticipant(userId, role);
         }
 
         public void RemoveParticipant(Guid userId) 
         {
-            var deletedUser = Participants.CoOrganizers.Remove(userId);
-            if(!deletedUser)
-            {
-                Participants.StandardParticipants.Remove(userId);
-            } 
+            Participants.RemoveParticipant(userId);
         }
+
+        public bool IsOrganizer(Guid userId) 
+            => OrganizerId == userId;
 
         public static Event CreateEvent(Guid id, Guid organizerId, string title, string description, DateTime startsAt, DateTime endsAt)
             => new Event(id, organizerId, title, description, startsAt, endsAt);
