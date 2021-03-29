57fe249ad5215cabb68c97d4e0ca88ba28c12750
cezary-kania <cezary.kaniadev@gmail.com>
2021-03-29 22:12:28 +0200

    Add in memory event repository

---
+++ MagicEvents.Api.Service/tests/MagicEvents.Api.Service.IntrationTests/InMemoryRepositories/InMemoryEventRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MagicEvents.Api.Service.Domain.Entities;
+using MagicEvents.Api.Service.Domain.Repositories;
+
+namespace MagicEvents.Api.Service.IntrationTests.InMemoryRepositories
+{
+    public class InMemoryEventRepository : IEventRepository
+    {
+        private ISet<Event> _events = new HashSet<Event>();
+        public Task CreateAsync(Event newEvent)
+        {
+            _events.Add(newEvent);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            var @event = _events.SingleOrDefault(x => x.Id == id);
+            _events.Remove(@event);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<Event>> GetAllAsync()
+        {
+            return Task.FromResult(_events.AsEnumerable()); 
+        }
+
+        public Task<Event> GetAsync(Guid id)
+        {
+            return Task.FromResult(_events.SingleOrDefault(x => x.Id == id));
+        }
+
+        public Task UpdateAsync(Event updatedEvent)
+        {
+            var existingEvent = Task.Run(async () => await GetAsync(updatedEvent.Id)).Result;
+            _events.Remove(existingEvent);
+            _events.Add(updatedEvent);
+            return Task.CompletedTask;
+        }
+    }
+}
