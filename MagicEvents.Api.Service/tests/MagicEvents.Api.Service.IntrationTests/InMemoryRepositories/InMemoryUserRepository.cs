178685401abb246684df31385fdafd8447f87dc9
cezary-kania <cezary.kaniadev@gmail.com>
2021-04-01 21:10:48 +0200

    Create InMemoryUserRepository

---
+++ MagicEvents.Api.Service/tests/MagicEvents.Api.Service.IntrationTests/InMemoryRepositories/InMemoryUserRepository.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MagicEvents.Api.Service.Domain.Entities;
+using MagicEvents.Api.Service.Domain.Repositories;
+
+namespace MagicEvents.Api.Service.IntrationTests.InMemoryRepositories
+{
+    public class InMemoryUserRepository : IUserRepository
+    {
+        private ISet<User> _users = new HashSet<User>();
+        
+        public Task AddAsync(User user)
+        {
+            _users.Add(user);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Guid userId)
+        {
+            var user = _users.SingleOrDefault(x => x.Id == userId);
+            _users.Remove(user);
+            return Task.CompletedTask;
+        }
+
+        public Task<User> GetAsync(Guid userId)
+        {
+            var user = _users.SingleOrDefault(x => x.Id == userId);
+            return Task.FromResult(user);
+        }
+
+        public Task<User> GetAsync(string userEmail)
+        {
+            var user = _users.SingleOrDefault(x => x.Identity.Email == userEmail);
+            return Task.FromResult(user);
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            var existingUser = Task.Run(async () => await GetAsync(user.Id)).Result;
+            _users.Remove(existingUser);
+            _users.Add(user);
+            return Task.CompletedTask;
+        }
+    }
+}
